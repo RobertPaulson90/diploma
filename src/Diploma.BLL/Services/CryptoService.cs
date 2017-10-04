e48dd7b230dacf36664ae12a5ecafe0db05869fa
Nesterenko Konstantin <nestquik@gmail.com>
2017-10-05 02:01:41 +0300

    Fully covered CryptoService

--- src/Diploma.BLL/Services/CryptoService.cs
+++ src/Diploma.BLL/Services/CryptoService.cs
@@ -8,6 +8,11 @@ namespace Diploma.BLL.Services
     {
         public string HashPassword(string password)
         {
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                throw new ArgumentNullException(nameof(password));
+            }
+
             const int HashSize = 16;
             const int Iterations = 1000;
             const int SaltSize = 16;
@@ -21,10 +26,57 @@ namespace Diploma.BLL.Services
 
         public bool VerifyPasswordHash(string password, string hashedPassword)
         {
-            var hashParts = hashedPassword.Split(':');
-            var iterations = int.Parse(hashParts[0]);
-            var originalSalt = Convert.FromBase64String(hashParts[1]);
-            var originalHash = Convert.FromBase64String(hashParts[2]);
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                throw new ArgumentNullException(nameof(password));
+            }
+
+            if (string.IsNullOrWhiteSpace(hashedPassword))
+            {
+                throw new ArgumentNullException(nameof(hashedPassword));
+            }
+
+            var hashParts = hashedPassword.Split(new[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (hashParts.Length != 3)
+            {
+                throw new FormatException($"The {nameof(hashedPassword)} must match the next format: \'iterations:salt:hash\'.");
+            }
+
+            var iterations = hashParts[0];
+            var originalSalt = hashParts[1];
+            var originalHash = hashParts[2];
+
+            int iterationsCount;
+            if (!int.TryParse(iterations, out iterationsCount))
+            {
+                throw new ArgumentException("Hash should contain iterations.", nameof(hashedPassword));
+            }
+
+            if (iterationsCount <= 1)
+            {
+                throw new ArgumentException("The number of iterations cannot be less than 1.", nameof(hashedPassword));
+            }
+
+            if (string.IsNullOrWhiteSpace(originalSalt))
+            {
+                throw new FormatException("Invalid salt.");
+            }
+
+            if (string.IsNullOrWhiteSpace(originalHash))
+            {
+                throw new FormatException("Invalid hash.");
+            }
+
+            var originalSaltBytes = Convert.FromBase64String(hashParts[1]);
+
+            var originalHashBytes = Convert.FromBase64String(hashParts[2]);
+
+            return VerifyPasswordHashInternal(password, iterationsCount, originalSaltBytes, originalHashBytes);
+        }
+
+        private static bool VerifyPasswordHashInternal(string password, int iterations, byte[] originalSalt, byte[] originalHash)
+        {
             using (var hashTool = new Rfc2898DeriveBytes(password, originalSalt, iterations))
             {
                 var hash = hashTool.GetBytes(originalHash.Length);
--- tests/Diploma.BLL.Tests/CryptoServiceTests.cs
+++ tests/Diploma.BLL.Tests/CryptoServiceTests.cs
@@ -1,16 +1,98 @@
-﻿using Diploma.BLL.Services;
+﻿using System;
+using Diploma.BLL.Services;
 using NUnit.Framework;
 
 namespace Diploma.BLL.Tests
 {
+    [TestFixture]
     public class CryptoServiceTests
     {
-        [TestCase("abc")]
-        public void VerifyPasswordHash(string password)
+        private CryptoService _cryptoService;
+
+        [TestCase("1q2w3e")]
+        public void HashPassword_Should_Return_Different_Hashes_For_Same_Password(string password)
         {
-            var service = new CryptoService();
-            var hash = service.HashPassword(password);
-            Assert.AreEqual(true, service.VerifyPasswordHash(password, hash));
+            var hash1 = _cryptoService.HashPassword(password);
+            var hash2 = _cryptoService.HashPassword(password);
+
+            Assert.AreNotEqual(hash1, hash2);
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void HashPassword_Should_Throw_ArgumentNullException_When_Password_Is_Null_Or_WhiteSpace(string password)
+        {
+            Assert.Throws<ArgumentNullException>(() => _cryptoService.HashPassword(password));
+        }
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _cryptoService = new CryptoService();
+        }
+
+        [TestCase("1q2w3e", "1000:grNq68etgzXX6tXDPaCmdA==: ")]
+        [TestCase("1q2w3e", "1000:grNq68etgzXX6tXDPaCmdA==:KioCDLN6rAHjc2KiEc/q5Q=%")]
+        public void VerifyPasswordHash_Should_Throw_ArgumentException_When_Hash_Is_Not_Valid_Base64_String(string password, string passwordHash)
+        {
+            Assert.Throws<FormatException>(() => _cryptoService.VerifyPasswordHash(password, passwordHash));
+        }
+
+        [TestCase("1q2w3e", "ddsd:grNq68etgzXX6tXDPaCmdA==:KioCDLN6rAHjc2KiEc/q5Q==")]
+        [TestCase("1q2w3e", " :grNq68etgzXX6tXDPaCmdA==:KioCDLN6rAHjc2KiEc/q5Q==")]
+        public void VerifyPasswordHash_Should_Throw_ArgumentException_When_Not_Contains_Iterations(string password, string passwordHash)
+        {
+            Assert.Throws<ArgumentException>(() => _cryptoService.VerifyPasswordHash(password, passwordHash));
+        }
+
+        [TestCase("1q2w3e", "0:grNq68etgzXX6tXDPaCmdA==:KioCDLN6rAHjc2KiEc/q5Q==")]
+        [TestCase("1q2w3e", "-1:grNq68etgzXX6tXDPaCmdA==:KioCDLN6rAHjc2KiEc/q5Q==")]
+        public void VerifyPasswordHash_Should_Throw_ArgumentException_When_Number_Of_Iterations_Less_Than_One(string password, string passwordHash)
+        {
+            Assert.Throws<ArgumentException>(() => _cryptoService.VerifyPasswordHash(password, passwordHash));
+        }
+
+        [TestCase("1q2w3e", "1000")]
+        [TestCase("1q2w3e", "1000:")]
+        [TestCase("1q2w3e", "1000:grNq68etgzXX6tXDPaCmdA==:")]
+        [TestCase("1q2w3e", "1000:grNq68etgzXX6tXDPaCmdA==:KioCDLN6rAHjc2KiEc/q5Q==:1000")]
+        public void VerifyPasswordHash_Should_Throw_ArgumentException_When_PasswordHash_Has_Wrong_Format(string password, string passwordHash)
+        {
+            Assert.Throws<FormatException>(() => _cryptoService.VerifyPasswordHash(password, passwordHash));
+        }
+
+        [TestCase("1q2w3e", "1000: :KioCDLN6rAHjc2KiEc/q5Q==")]
+        [TestCase("1q2w3e", "1000:grNq68etgzXX6tXDPaCmdA=%:KioCDLN6rAHjc2KiEc/q5Q==")]
+        public void VerifyPasswordHash_Should_Throw_ArgumentException_When_Salt_Is_Not_Valid_Base64_String(string password, string passwordHash)
+        {
+            Assert.Throws<FormatException>(() => _cryptoService.VerifyPasswordHash(password, passwordHash));
+        }
+
+        [TestCase(null, "1000:grNq68etgzXX6tXDPaCmdA==:KioCDLN6rAHjc2KiEc/q5Q==")]
+        [TestCase("", "1000:grNq68etgzXX6tXDPaCmdA==:KioCDLN6rAHjc2KiEc/q5Q==")]
+        [TestCase(" ", "1000:grNq68etgzXX6tXDPaCmdA==:KioCDLN6rAHjc2KiEc/q5Q==")]
+        public void VerifyPasswordHash_Should_Throw_ArgumentNullException_When_Password_Is_Null_Or_WhiteSpace(string password, string passwordHash)
+        {
+            Assert.Throws<ArgumentNullException>(() => _cryptoService.VerifyPasswordHash(password, passwordHash));
+        }
+
+        [TestCase("1q2w3e", null)]
+        [TestCase("1q2w3e", "")]
+        [TestCase("1q2w3e", " ")]
+        public void VerifyPasswordHash_Should_Throw_ArgumentNullException_When_PasswordHash_Is_Null_Or_WhiteSpace(
+            string password,
+            string passwordHash)
+        {
+            Assert.Throws<ArgumentNullException>(() => _cryptoService.VerifyPasswordHash(password, passwordHash));
+        }
+
+        [TestCase("1q2w3e", "1000:grNq68etgzXX6tXDPaCmdA==:KioCDLN6rAHjc2KiEc/q5Q==")]
+        public void VerifyPasswordHash_Should_Verify_Password_Against_Valid_Hash(string password, string passwordHash)
+        {
+            var isHashMatchPassword = _cryptoService.VerifyPasswordHash(password, passwordHash);
+
+            Assert.True(isHashMatchPassword);
         }
     }
 }
