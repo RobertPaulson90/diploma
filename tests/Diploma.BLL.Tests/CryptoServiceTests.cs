9c4b40ace75a14150d5886f5801044fcf6d9e6d2
Johan Larsson <johan_gavle1@hotmail.com>
2017-10-04 18:00:28 +0200

    Test project with sample test.

---
+++ tests/Diploma.BLL.Tests/CryptoServiceTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Diploma.BLL.Tests
+{
+    using Diploma.BLL.Services;
+    using NUnit.Framework;
+
+    public class CryptoServiceTests
+    {
+        [TestCase("abc")]
+        public void VerifyPasswordHash(string password)
+        {
+            var service = new CryptoService();
+            var hash = service.HashPassword(password);
+            Assert.AreEqual(true, service.VerifyPasswordHash(password, hash));
+        }
+    }
+}
---
+++ tests/Diploma.BLL.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Diploma.BLL.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Diploma.BLL.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e4b35c9d-76f8-45c3-86d5-1ec68f7939ab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
